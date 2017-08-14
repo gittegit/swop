da317c2deb83cebc16d250d073c73a478671bec5
brigittekwasny@gmail.com <b.kwasny@statusgreen.de>
2017-08-14 15:02:11 +0200

    add courseList to FE

--- src/components/SwopcardBackend.vue
+++ src/components/SwopcardBackend.vue
@@ -4,6 +4,7 @@
 
 <script type="text/babel">
   import db from 'baqend'
+  import vue from 'vue'
 
   export default {
     data () {
@@ -15,6 +16,14 @@
       created () {
         this.username = db.User.me.username
         this.course
+        console.log(this.username)
+        db.Course.find().resultList((result) => {
+          result.forEach((course) => {
+            vue.log(course)
+            this.course.push(course)
+            console.log(this.course)
+          })
+        })
       },
       beforeRouteEnter (to, from, next) {
         if (!db.User.me) {
