62f9c59aaa87c6395981a4919a32ef8ac76ed439
brigittekwasny@gmail.com <b.kwasny@statusgreen.de>
2017-08-15 15:14:34 +0200

    isValid() does not rly work

--- src/components/AddCourse.vue
+++ src/components/AddCourse.vue
@@ -9,7 +9,7 @@
         <input type="text" v-model.trim="id" class="form-control" name="id" required>
         <label>Name</label>
         <input type="text" v-model.trim="name" class="form-control" name="name" required>
-        <button class="btn btn-info" type="button" @click="addCourse" :disabled.prop="!isValid">Add Course</button>
+        <button class="btn btn-info" type="button" @click="addCourse"><!-- :disabled.prop="!isValid" -->Add Course</button>
       </div>
     </div>
   </div>
@@ -29,27 +29,27 @@
     },
     methods: {
       addCourse () {
-        db.Course.create(this.id, this.name).then(() => {
-          console.log('yay')
-        }).catch(e => {
-          this.error = e.message
-        })
+//        db.Course.create(this.id, this.name).then(() => {
+//          console.log('yay')
+//        }).catch(e => {
+//          this.error = e.message
+//        })
       }
     },
-    computed: {
-      isValid () {
-        if (this.id.length > 1 && this.name.length > 1 && this.id) {
-          var queryBuilder = db.Course.find()
-          var conditionId = queryBuilder.equal('id', '/db/Course/' + this.id)
-          var conditionName = queryBuilder.equal('name', this.name)
-          queryBuilder.or(conditionId, conditionName)
-            .resultList().then((course) => {
-              console.log(course.length === 0)
-              return (course.length === 0)
-            })
-        }
-      }
-    },
+//    computed: {
+//      isValid () {
+//        if (this.id.length > 1 && this.name.length > 1 && this.id) {
+//          var queryBuilder = db.Course.find()
+//          var conditionId = queryBuilder.equal('id', '/db/Course/' + this.id)
+//          var conditionName = queryBuilder.equal('name', this.name)
+//          queryBuilder.or(conditionId, conditionName)
+//            .resultList().then((course) => {
+//              console.log(course.length === 0)
+//              return (course.length === 0)
+//            })
+//        }
+//      }
+//    },
     beforeRouteEnter (to, from, next) {
       if (!db.User.me) {
         next('signup')
