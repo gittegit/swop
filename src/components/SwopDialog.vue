3fbc98ac5c8f938fa474e77f44721b6c51a8af3e
Patricia Häußer <patriciahausser@dhcp-172-21-85-8.wlan.uni-hamburg.de.local>
2017-08-16 11:33:28 +0200

    Add addCourse feature

--- src/components/SwopDialog.vue
+++ src/components/SwopDialog.vue
@@ -1,97 +1,152 @@
 <template>
-<div class="container main-content">
+<div class="main-content">
+  <div class="container">
 
-  <!-- Inhalt / Formulare -->
-  <div class="columns is-centered">
+    <!-- Inhalt / Formulare -->
+    <div class="columns is-centered">
 
-    <div class="column is-9 is-narrow is-form">
+      <div class="column is-9 is-narrow is-form">
 
-      <h1 class="title is-3 has-text-centered">Dein swop</h1>
+        <h1 class="title is-3 has-text-centered">Dein swop</h1>
 
-      <!-- Stepwise-Naviagtion / Fortschrittsanzeige -->
-      <ul class="nav-dots-container">
-        <li class="nav-dot" :class="{ 'done': firstStepDone}"></li>
-        <li class="nav-dot" :class="{ 'done': secondStepDone}"></li>
-        <li class="nav-dot" :class="{ 'done': thirdStepDone}"></li>
-      </ul>
-      <!-- / Stepwise-Naviagtion / Fortschrittsanzeige -->
+        <!-- Stepwise-Naviagtion / Fortschrittsanzeige -->
+        <ul class="nav-dots-container">
+          <li class="nav-dot" :class="{ 'done': firstStepDone}"></li>
+          <li class="nav-dot" :class="{ 'done': secondStepDone}"></li>
+          <li class="nav-dot" :class="{ 'done': thirdStepDone}"></li>
+        </ul>
+        <!-- / Stepwise-Naviagtion / Fortschrittsanzeige -->
 
-      <!-- Step 1 -->
-      <div v-if="firstStepActive">
-        <h4 class="description is-5 has-text-centered">Aus welcher Veranstaltung möchtest Du <strong>heraus</strong> wechseln?</h4>
+        <!-- Step 1 -->
+        <div v-if="firstStepActive && !newCourse">
+          <h4 class="description is-5 has-text-centered">Aus welcher Veranstaltung möchtest Du <strong>heraus</strong> wechseln?</h4>
 
-        <!-- Input -->
-        <b-field>
-          <b-autocomplete v-model="arrayExample.name" :data="filteredDataArray" placeholder="Deine aktuelle Veranstaltung" @select="option => arrayExample.selected = option">
-          </b-autocomplete>
-        </b-field>
-
-        <!-- Untergruppe hinzufügen-Label (nur wenn noch keine hinzugefügt)-->
-        <p v-if="!hasSubgroup" :class="{ 'help': true, 'add-group': true}" @click="addGroup">+ Untergruppe hinzufügen</p>
-
-        <!-- Untergruppe hinzufügen-Input (nur bei Klick auf Label)-->
-        <div v-if="hasSubgroup">
-          <b-field has-addons>
-            <p class="control is-expanded">
-              <b-input placeholder="Deine aktuelle Untergruppe"></b-input>
-            </p>
-            <p class="control">
-              <a class="button" @click="removeGroup">
-                <span class="icon is-small">
-                  <i class="fa fa-trash-o"></i>
-                </span>
-            </a>
-            </p>
+          <!-- Input -->
+          <b-field>
+            <b-autocomplete v-model="arrayExample.name" :data="filteredDataArray" placeholder="Deine aktuelle Veranstaltung" @select="option => arrayExample.selected = option">
+            </b-autocomplete>
           </b-field>
-        </div>
-      </div>
-      <!-- / Step 1 -->
 
-
-      <!-- Step 2 -->
-      <div v-if="secondStepActive">
-        <h4 class="description is-5 has-text-centered">In welche Veranstaltung möchtest Du <strong>hinein</strong> wechseln?</h4>
-
-        <!-- Input -->
-        <b-field>
-          <b-autocomplete v-model="arrayExample.name" :data="filteredDataArray" placeholder="Deine Wunschveranstaltung" @select="option => arrayExample.selected = option">
-          </b-autocomplete>
-        </b-field>
-
-
-        <!-- Untergruppe hinzufügen-Input (nur bei Klick auf Label)-->
-        <div v-if="hasSubgroup">
-
-          <b-field has-addons>
-            <p class="control is-expanded">
-              <b-input placeholder="Deine Wunsch-Untergruppen"></b-input>
-            </p>
-            <p class="control">
-              <a class="button" @click="removeGroup">
-                <span class="icon is-small">
-                  <i class="fa fa-trash-o"></i>
-                </span>
-            </a>
-            </p>
-          </b-field>
           <!-- Untergruppe hinzufügen-Label (nur wenn noch keine hinzugefügt)-->
-        </div>
+          <p v-if="!hasCourseGroupFrom" :class="{ 'help': true, 'add-group': true}" @click="addGroup">
+            + Untergruppe hinzufügen
+          </p>
 
-        <p v-if="hasSubgroup":class="{ 'help': true, 'add-group': true}" @click="addGroup">+ Untergruppe hinzufügen</p>
+          <!-- Untergruppe hinzufügen-Input (nur bei Klick auf Label)-->
+          <div v-if="hasCourseGroupFrom">
+            <b-field has-addons>
+              <p class="control is-expanded">
+                <b-input placeholder="Deine aktuelle Untergruppe"></b-input>
+              </p>
+              <p class="control">
+                <a class="button" @click="removeGroup">
+                <span class="icon is-small">
+                  <i class="fa fa-trash-o"></i>
+                </span>
+            </a>
+              </p>
+            </b-field>
+          </div>
+        </br>
+          <p :class="{ 'help': true, 'has-text-primary': true}" @click="addCourse">
+            Deine Veranstaltung ist nicht dabei?
+          </p>
+        </div>
+        <!-- / Step 1 -->
+
+
+        <!-- Step 1.2 Course erstellen -->
+        <div v-if="firstStepActive && newCourse">
+          <h4 class="description is-5 has-text-centered">Aus welcher Veranstaltung möchtest Du <strong>heraus</strong> wechseln?</h4>
+
+          <!-- Input -->
+          <b-field>
+            <p class="control is-expanded">
+              <b-input placeholder="Vollständiger Name Deiner aktuellen Veanstaltung" v-model="courseTitleFrom"></b-input>
+            </p>
+          </b-field>
+          <b-field>
+            <p class="control is-expanded">
+              <b-input placeholder="ID Deiner aktuellen Veranstaltung" v-model="courseIdFrom"></b-input>
+            </p>
+          </b-field>
+
+
+          <!-- Untergruppe hinzufügen-Label (nur wenn noch keine hinzugefügt)-->
+          <p v-if="!hasCourseGroupFrom" :class="{ 'help': true, 'add-group': true}" @click="addGroup">
+            + Untergruppe hinzufügen
+          </p>
+
+          <!-- Untergruppe hinzufügen-Input (nur bei Klick auf Label)-->
+          <div v-if="hasCourseGroupFrom">
+            <b-field has-addons>
+              <p class="control is-expanded">
+                <b-input placeholder="Deine aktuelle Untergruppe"></b-input>
+              </p>
+              <p class="control">
+                <a class="button" @click="removeGroup">
+                <span class="icon is-small">
+                  <i class="fa fa-trash-o"></i>
+                </span>
+            </a>
+              </p>
+            </b-field>
+          </div>
+        </div>
+        <!-- / Step 1.2 Course erstellen -->
+
+
+        <!-- Step 2 -->
+        <div v-if="secondStepActive">
+          <h4 class="description is-5 has-text-centered">In welche Veranstaltung möchtest Du <strong>hinein</strong> wechseln?</h4>
+
+          <!-- Input -->
+          <b-field>
+            <b-autocomplete v-model="arrayExample.name" :data="filteredDataArray" placeholder="Deine Wunschveranstaltung" @select="option => arrayExample.selected = option">
+            </b-autocomplete>
+          </b-field>
+
+
+          <!-- Untergruppe hinzufügen-Input-->
+          <div v-if="hasCourseGroupFrom && canHaveCourseGroupTo" v-for="courseCroupTo in courseGroupToArray">
+
+            <b-field has-addons>
+              <p class="control is-expanded">
+                <b-input placeholder="Deine Wunsch-Untergruppen"></b-input>
+              </p>
+              <p class="control">
+                <a class="button" @click="removeGroup">
+                <span class="icon is-small">
+                  <i class="fa fa-trash-o"></i>
+                </span>
+            </a>
+              </p>
+            </b-field>
+            <!-- Untergruppe hinzufügen-Label (nur wenn noch keine hinzugefügt)-->
+          </div>
+
+          <p v-if="canHaveCourseGroupTo" :class="{ 'help': true, 'add-group': true}" @click="addGroup">+ Untergruppe hinzufügen</p>
+
+        </div>
+        <!-- / Step 2 -->
+
+        <!-- Step 3 -->
+        <!-- TO DO -->
+        <!-- / Step 3 -->
+
+
+        <!-- Button-Group Navigation -->
+        <button-group>
+          <div slot="backItem">
+            <router-link v-if="firstStepActive" @click="back" :to="backLink">{{backItem}}</router-link>
+            <div v-if="!firstStepActive" @click="back">{{backItem}}</div>
+          </div>
+          <div slot="forwardItem" @click="forward">Weiter</div>
+        </button-group>
+        <!-- / Button-Group Navigation -->
+
 
       </div>
-      <!-- / Step 2 -->
-
-
-      <!-- Button-Group Navigation -->
-      <button-group>
-        <div slot="backItem">
-          <router-link v-if="firstStepActive" @click="back" :to="backLink">{{backItem}}</router-link>
-          <div v-if="!firstStepActive" @click="back">{{backItem}}</div>
-        </div>
-        <div slot="forwardItem" @click="forward">Weiter</div>
-      </button-group>
-
     </div>
   </div>
 </div>
@@ -108,16 +163,26 @@ export default {
       firstStepDone: true, // Stepweise-Anzeiger erste Seite
       secondStepDone: false, // Stepweise-Anzeiger erste Seite
       thirdStepDone: false, // Stepweise-Anzeiger erste Seite
-      hasSubgroup: false, // Untergruppe in aktueller Veranstaltung?
       firstStepActive: true, // Inhalt Step 1
       secondStepActive: false, // Inhalt Step 2
       thirdStepActive: false, // Inhalt Step 3
+      newCourse: false, // Suche in vorhandenen Kursen oder neue Kurserstellung?
+      courseTitleFrom: '', // Titel der aktuellen Veranstaltung
+      courseIdFrom: '', // ID der aktuellen Veranstaltung
+      courseTitleTo: '', // Titel der gewünschten Veranstaltung
+      courseIdTo: '', // ID der gewünschten Veranstaltung
+      courseGroupFrom: '', // Untergruppe der aktuellen Veranstaltung
+      courseGroupTo: '', // Untergruppe der gewünschten Veranstaltung (gesamte Liste in courseGroupToArray)
+      hasCourseGroupFrom: false, // itital
+      hasCourseGroupTo: false, // initial
+      canHaveCourseGroupTo: false, // initial
       msg: 'Welcome to Your Vue.js and Baqend App',
       isLoggedIn: null,
       backItem: 'Abbrechen', // Beschriftung für zurück-Button in Button-Group (inital: Abbrechen)
       backLink: {
         name: 'dashboard' // Link von zurück-Button in Button-Group (initial: Dashboard)
       },
+      subgroupItem: '',
       // --- Daten für Autocomplete ---
       arrayExample: {
         data: [
@@ -137,17 +202,35 @@ export default {
         ],
         name: '',
         selected: null
-      }
+      },
+      courseGroupToArray: [
+      ]
       // /--- Daten für Autocomplete ---
     }
   },
 
   methods: {
+    addCourse () {
+      this.newCourse = true
+    },
     addGroup () {
-      this.hasSubgroup = true
+      if (this.firstStepActive) {
+        this.hasCourseGroupFrom = true
+        this.canHaveCourseGroupTo = true
+        this.courseGroupToArray.push(null)
+        console.log(this.courseGroupToArray)
+        console.log('Länge: ' + this.courseGroupToArray.length)
+      } else if (this.secondStepActive) {
+        this.hasCourseGroupTo = true
+      }
     },
     removeGroup () {
-      this.hasSubgroup = false
+      if (this.firstStepActive) {
+        this.hasCourseGroupFrom = false
+        this.canHaveCourseGroupTo = false
+      } else if (this.secondStepActive) {
+        this.hasCourseGroupTo = false
+      }
     },
     forward () { // Aktionen bei Klick auf Weiter-Button
       console.log('hi')
@@ -159,6 +242,12 @@ export default {
         this.secondStepDone = true
         // Zurück-Button setzen
         this.backItem = 'Zurück'
+        // Eingaben speichern
+        if (!this.newCourse) {
+          this.courseTitleFrom = this.arrayExample.name
+        }
+        console.log(this.courseTitleFrom)
+        console.log(this.courseIdFrom)
       } else if (this.firstStepDone && this.secondStepDone) {
         // Nav-Dots setzen
         this.secondStepActive = false
