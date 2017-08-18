3554639383a1c916526af59016739a7f1ec89ef0
brigittekwasny@gmail.com <b.kwasny@statusgreen.de>
2017-08-18 14:09:47 +0200

    delete swopCards command

--- src/components/Me.vue
+++ src/components/Me.vue
@@ -48,13 +48,6 @@ export default {
   created () {
     this.username = db.User.me.username
     this.$parent.isLoggedIn = true
-    var swopCard1 = 'b61c5f87-c3fe-43fa-a253-cdffa60733a6'
-    var swopCard2 = 'b4705fc2-eac6-4ac0-9821-4a2f9e57c3f8'
-    m.createMatch(swopCard1, swopCard2).then((result) => {
-      console.log(result)
-    }).catch((err) => {
-      console.log(err)
-    })
   },
   beforeRouteEnter (to, from, next) {
     if (!db.User.me) {
