a82b4afbcf127fd5ca693fb4c6254140b50fd8c1
lukasuz <5uzolas@informatik.uni-hamburg.de>
2017-08-25 13:07:10 +0200

    Kommentare geschrieben. Console.logs entfernt

--- src/Model/model.js
+++ src/Model/model.js
@@ -1,40 +1,56 @@
 import db from 'baqend'
 
 /**
-Das Modell dient als Schnittstelle zur Datenbank
+* Das Model dient als Schnittstelle zur Datenbank, es stellt Getter und Setter
+* Methoden zur Manipulation der User, SwopCard, Course und Match Daten bereit.
+* Es hält ein lokales Abbild der Userdaten als Felder, die jedes Mal Aktualisiert
+* werden, wenn eine neue Anfrage beispielsweise mittels createSwopCard()
+* gestellt wird. Die Daten können zudem mittels initUserData() und refreshHard()
+* manuell neu geladen werden.
 */
 class Modul {
   constructor () {
-    this.swopCards
-    this.user
-    this.matches
+    this.swopCards //{Map}
+    this.user //{Object}
+    this.matches //{Map}
+    // db.connect('restless-bolt-1') Wird momentan in der main.js geladen
   }
 
   /**
-  * Zieht User Daten vom Server, auch wenn schon etwas geladen war
-  *
+  * Versucht Serverdaten vom Server zu laden. "Versucht", weil anscheinend
+  * Userdaten gecached werden und nicht jedes mal eine neue Anfrage rausgesendet
+  * wird.
+  * @returns {Promise|Object}
   */
   refreshHard () {
     return db.User.load(db.User.me.id, {depth: 3})
       .then((user) => {
         this.parseAndSaveUserData(user)
+        return new Promise(function (resolve, reject) {
+          ({message: 'Userdaten wurden neu geladen'})
+        })
       })
   }
 
   /**
-  * Initialisiert die User Daten einmal zu Begin
-  * @returns {Promise}
+  * Lädt Userdaten vom Server, sofern diese noch nicht initialisiert waren
+  * @returns {Promise|Object}
   */
   initUserData () {
-    if (this.user || this.matches || this.swopCards) {
+    if (this.user) {
       return new Promise(function (resolve, reject) {
-        resolve('Userdaten waren schon initialisiert')
+        resolve({message: 'Userdaten waren schon geladen'})
       })
     } else {
       return this.refreshHard()
     }
   }
 
+  /**
+  * Aktualisiert die lokalen Userdaten, d.h. this.user, this.swopCards und
+  * this.matches
+  * @param {currentUser} User mit den zuaktualisierenden Daten
+  */
   parseAndSaveUserData (currentUser) {
     this.user = currentUser
     const swopTemp = new Map()
@@ -49,16 +65,22 @@ class Modul {
     this.matches = matchesTemp
   }
 
+  /**
+  * Aktualisiert die lokalen restrictedUserInfo Daten, d.h. Emailadresse und
+  * Displaynamen
+  * @param {restrictedUserInfo} restrictedUserInfo die die zuaktualisierenden
+  * Daten enthält
+  */
   parseAndSaveRestrictedUserInfoData (restrictedUserInfo) {
     this.user.restrictedUserInfo.displayName = restrictedUserInfo.displayName
     this.user.restrictedUserInfo.email = restrictedUserInfo.email
   }
 
   /**
-  Loggt den User ein
-  @param username: der username (Emailadresse) des Users
-  @param pw: password des Users
-  @return Promise
+  * Loggt den User ein
+  * @param {username} String der username (Emailadresse) des Users
+  * @param {pw} String Passwort des Users
+  * @return {Promise}
   */
   login (username, pw) {
     username = username.toLowerCase()
@@ -66,8 +88,8 @@ class Modul {
   }
 
   /**
-  Loggt den User aus
-  @return Promise
+  * Loggt den User aus
+  * @return {Promise}
   */
   logout () {
     this.user = null
@@ -77,41 +99,54 @@ class Modul {
   }
 
   /**
-   * registriert einen neuen User
-   * @param displayName
-   * @param username
-   * @param password
-   * @returns {*|Promise.<model.User>}
+   * Registriert einen neuen User
+   * @param {displayName} String der gewünschte Displayname des Users
+   * @param {username} String die Emailadresse des Users
+   * @param {password} String das gewünschte Passwort des Users
+   * @returns {Promise|Object}
    */
   register (displayName, username, password) {
     username = username.toLowerCase()
-    var restrictedUserInfo = new db.RestrictedUserInfo({email: username, displayName: displayName})
+    const restrictedUserInfo = new db.RestrictedUserInfo({
+      email: username,
+      displayName: displayName
+    })
     return restrictedUserInfo.save().then(() => {
       return db.User.register(username, password)
     })
-    // return db.modules.post('createUser', {username: username, password: password, displayName: displayName})
-      // return db.modules.post('updateRestrictedUserInfo', {id: userObject.id, displayName: displayName})
   }
 
+  /**
+  * Gibt den Displaynamen des eingeloggten Users aus
+  * @returns {String} der Displaynamen des eingeloggten Users
+  */
   getDisplayName () {
     return this.user.restrictedUserInfo.displayName
   }
 
+  /**
+  * Gibt die Emailadresse eingeloggten Users aus
+  * @returns {String} die Emailadresse des eingeloggten Users
+  */
   getEmail () {
     return this.user.restrictedUserInfo.email
   }
 
+  /**
+  * Gibt den usernamen des eingeloggten Users aus
+  * @returns {String} der username des eingeloggten Users
+  */
   getLoginEmail () {
     return this.user.username
   }
 
   /**
-   * updates the displayName of the user
-   * @param displayName
-   * @returns {Promise.<*>}
+   * Updatet den Displaynamen des aktuellen Users.
+   * @param {email} String der gewünschte neue Name
+   * @returns {Promise|JSON} welches angibt ob die Aktion erfolgreich war und das
+   * aktualisierte RestrictedUserInfo Objekt enthält
    */
   updateUsername (displayName) {
-    // return db.modules.post('updateDisplayName', {displayName: displayName})
     return db.modules.post('UserService', {route: 'UPDATE_DISPLAY_NAME', displayName: displayName})
       .then((val) => {
         this.parseAndSaveRestrictedUserInfoData(val.success.updatedRestrictedUserInfo)
@@ -122,9 +157,11 @@ class Modul {
   }
 
   /**
-   * Updatet die Email an die die Swopbenachrichtigungen geschickt werden und
-   * @param email
-   * @returns {Promise.<*>}
+   * Updatet die Email an die die Swopbenachrichtigungen geschickt werden in
+   * der RestrictedUserInfo.
+   * @param {email} String die gewünschte neue Email
+   * @returns {Promise|JSON} welches angibt ob die Aktion erfolgreich war und das
+   * aktualisierte RestrictedUserInfo Objekt enthält
    */
   updateEmail (email) {
     email = email.toLowerCase()
@@ -138,37 +175,38 @@ class Modul {
   }
 
   /**
-   * Deletes the user for EVER
-   * @returns {*|{value}}
+   * Löscht den User aus der Datenbank. Schmeißt im Moment noch gewollt einen
+   * Error.
+   * @returns {Promise|Object}
    */
   deleteUser () {
     return db.modules.post('UserService', {route: 'DELETE_USER'})
   }
 
   /**
-   * change the password
-   * @param {String} oldPassword
-   * @param {String} newPassword
-   * @returns {*|{value}}
+   * Ändert das Passwort des aktuellen Users
+   * @param {String} oldPassword das aktuelle Passwort des Users
+   * @param {String} newPassword das neue Passwort des Users
+   * @returns {Promise|Object}
    */
   changePassword (oldPassword, newPassword) {
     return db.User.newPassword(db.User.me.username, oldPassword, newPassword)
   }
 
   /**
-   * Create a new course
-   * @param name String
-   * @param courseId String
-   * @returns {Promise.<*>}
+   * Erstellt einen neuen Kurs
+   * @param {name} String Name der zuerstellenden Veranstaltung
+   * @param {courseId} String Veranstaltungsid der
+   * @returns {Promise|JSON}
    */
   createCourse (name, courseId) {
     return db.modules.post('createCourse', {id: courseId, name: name})
   }
 
   /**
-   * Get all courses from the database
-   * @return Promise
-   */
+  * Gibt alle aktuell in Datenbank gespeicherten Kurse in der Datenbank aus.
+  * @return {Promise|JSON}
+  */
   getAllCourses () {
     return db.Course.find()
       .ascending('name')
@@ -179,13 +217,15 @@ class Modul {
   }
 
   /**
-  * Erstellt eine Swopcard und speichert Sie in der Datenbank ab
-  @param courseId: String, die Id des Kurses
-  @param group: String, optional, die Gruppe in der sich der User befindet
-  @param searchedCourses: [String], Ids der Kurse die gesucht werden
-  @param searchedGroups: [String], Gruppennamen der Gruppen die gesucht werden
-  @throws Error sofern ein Kurs nicht existiert
-  @return Promise
+  * Erstellt eine neue Swopcard und speichert Sie in der Datenbank ab
+  * @param {searchedCourses} Array Ids als Strings der Kurse die gesucht werden
+  * @param {searchedGroups} Array Gruppennamen als Strings der Gruppen die
+  * gesucht werden, wenn keine gesucht werden leeres Array [] übergeben
+  * @param {courseId} String die Id des Veranstaltung aus der User raus will
+  * @param {group|String} String die Gruppe in der sich der User befindet, wenn
+  * keine Gruppen existiert leeren String "" übergeben.
+  * @return {Promise|JSON} welches angibt ob die Aktion erfolgreich war und das
+  * aktualisierte User Objekt enthält
   */
   createSwopCard (searchedCourses, searchedGroups, courseId, group) {
     return db.modules.post('SwopCardService', {
@@ -196,65 +236,49 @@ class Modul {
       group: group
     })
     .then((val) => {
-      const parsedValue = JSON.parse(JSON.stringify(val))
-      this.parseAndSaveUserData(parsedValue.success.user)
-      this.getMySwopCards().then((e) => {
-        return new Promise(function (resolve, reject) {
-          // console.log('M nach erstellung von Swopcard', M)
-          // console.log('swopcards nach der Erstellung', e)
-          resolve(val)
-        })
+      // const parsedValue = JSON.parse(JSON.stringify(val))
+      this.parseAndSaveUserData(val.success.user)
+      return new Promise(function (resolve, reject) {
+        resolve(val)
       })
+      // this.getMySwopCards().then((e) => {
+      //   return new Promise(function (resolve, reject) {
+      //     resolve(val)
+      //   })
+      // })
     })
-    // .then((val) => {
-    //   console.log('Swopcard erstellt', val) // TODO: WTF WTF WTF
-    //   promiseValue = val
-    //   return this.loadUserData()
-    // })
-    // .then(() => {
-    //   console.log('Swopcard erstellt', this.swopCards)
-    //   return new Promise(function (resolve, reject) {
-    //     resolve(promiseValue)
-    //   })
-    // })
   }
 
   /**
-  *Returns a Swopcard
-  *@param swopCardId id der Swopcard
-  *@returns Swopcard
+  * Gibt eine bestimmte SwopCard aus
+  * @param {swopCardId} String: Id der SwopCard, die ausgegeben werdne soll
+  * @returns {SwopCard}
   */
   getSwopCard (swopCardId) {
     return this.swopCards.get(swopCardId)
   }
 
   /**
-  *Legacy Function, returns a swopcard
-  *
+  * Gibt alle SwopCards des aktuellen Users aus. War mal asynchron, deswegen
+  * wird hier noch ein Promise zurückgegben.
+  * @returns {Promise} Enthält die SwopCards als sortiertes Array
   */
-  loadSwopCardById (swopCardId) {
-    return this.getSwopCard(swopCardId)
-  }
-
-  /**
-   * get all swopCards from the current User
-   * @returns {Promise} containing the Swopcards as an Array
-   */
   getMySwopCards () {
     const swoppies = Array.from(this.swopCards.values())
+    // TODO: nicht jedes mal sortieren
     swoppies.sort(function (a, b) {
       return b.createdAt.getTime() - a.createdAt.getTime()
     })
     return new Promise(function (resolve, reject) {
-      // console.log('getMySwopCards 3')
       resolve(swoppies)
     })
   }
 
   /**
-   * Deletes a swopcard by its given id
-   * @param id of the swopCard
-   * @returns {Promise.<*>}
+   * Löscht eine SwopCard des aktuellen Users
+   * @param {id} String die Id der SwopCard die gelöscht werden soll
+   * @returns {Promise|JSON} welches angibt ob die Aktion erfolgreich war und das
+   * aktualisierte User Objekt enthält
    */
   deleteSwopCard (id) {
     return db.modules.post('SwopCardService', {
@@ -270,21 +294,19 @@ class Modul {
   }
 
   /**
-    * Überprüft LOKAL ob der User zu einem gegebenen Match den Status verändern
-    * kann, also ob der User den Match schon accepted oder declined hat
-    * @return {boolean} true, if the user has not accepted/declined yet, false
-    * if the user has already accepted/declined a match
-    * @throws {Error}
+  * Überprüft ob der User zu einem gegebenen Match den Status verändern
+  * kann, also ob der User den Match schon akzeptiert oder abgelehnt hat.
+  * @param {matchId} String die Id des Matches für die überprüft werden soll,
+  * ob der Status mittels des aktuellen Users änderbar ist.
+  * @return {boolean} true, falls der User noch nicht akzeptiert/abgelehnt
+  * hat, sonst false.
   */
   isMatchStatusChangeable (matchId) {
     let matchObj
     // Es ist egal ob der Pfad oder nur die Id übergeben wird
     if (!matchId.startsWith('/db/Match/')) matchId = 'db/Match/' + matchId
-    // find the right MatchObj
+    // get the right match
     matchObj = this.matches.get(matchId)
-    // if (!matchObj) throw 'No match was found with that id that belongs to you'
-    // find out if db.User.me is user1 oder user2 and check that status
-    // console.log(matchObj, db.User.me.id)
     if (matchObj.user1.user.id === db.User.me.id) {
       return matchObj.status1 === 'WAITING'
     } else if (matchObj.user2.user.id === db.User.me.id) {
@@ -293,19 +315,18 @@ class Modul {
   }
 
   /**
-    * Gibt lokal den Status eines Matches aus
-    *@return {String[]} wobei der erste Eintrag der Match Status des eigenen
-    * Users ist und der zweite der Match Status des anderen Users ist
-    * @throws {Error}
+  * Gibt die aktuellen Stati beider User eines Matches aus.
+  * @param {matchId} String die Id des Matches für die beide Stati ausgegeben
+  *  werden sollen.
+  * @return {String[]} wobei der erste Eintrag der Match Status des eigenen
+  * Users ist und der zweite der Match Status des anderen Users ist.
   */
   getMatchStatus (matchId) {
     let matchObj
     // Es ist egal ob der Pfad oder nur die Id übergeben wird
     if (!matchId.startsWith('/db/Match/')) matchId = 'db/Match/' + matchId
-    // find the right MatchObj
+    // get the right MatchObj
     matchObj = this.matches.get(matchId)
-    // if (!matchObj) throw 'No match was found with that id that belongs to you'
-    // find out if db.User.me is user1 oder user2
     if (matchObj.user1.user.id === db.User.me.id) {
       return [matchObj.status1, matchObj.status2]
     } else if (matchObj.user2.user.id === db.User.me.id) {
@@ -313,13 +334,19 @@ class Modul {
     }
   }
 
+  /**
+  * Gibt das RestrictedUserInfo Objekt des anderen Users eines Matches aus,
+  * sofern dieses schon vom MatchService gesetzt wurde.
+  * @param {matchId} String die Id des Matches für die das RUI des anderen
+  * Users ausgegeben werden soll
+  * @returns {RestrictedUserInfo} ({displayName: String, email: String})
+  */
   getMatchUserDetail (matchId) {
     let matchObj
     // Es ist egal ob der Pfad oder nur die Id übergeben wird
     if (!matchId.startsWith('/db/Match/')) matchId = 'db/Match/' + matchId
     // find the right MatchObj
     matchObj = this.matches.get(matchId)
-
     if (matchObj.user1.user.id === db.User.me.id) {
       return matchObj.user2.restrictedUserInfo
     } else if (matchObj.user2.user.id === db.User.me.id) {
@@ -328,12 +355,12 @@ class Modul {
   }
 
   /**
-   * accepts the match from db.User.me by the given match Id
-   * @param matchId
-   * @returns {Promise.<*>}
+   * Akzeptiert den Match zu einer gegebenen MatchId vom aktuellen User aus ab
+   * @param {matchId} String die Id des Matches
+   * @returns {Promise|JSON} welches angibt ob die Aktion erfolgreich war und das
+   * aktualisierte User Objekt enthält
    */
   acceptMatch (matchId) {
-    // return db.modules.post('acceptMatch', {id: matchId})
     return db.modules.post('MatchService', {
       route: 'ACCEPT_MATCH',
       id: matchId
@@ -347,12 +374,12 @@ class Modul {
   }
 
   /**
-   * declines the match from db.User.me by the given match Id
-   * @param matchId
-   * @returns {Promise.<*>}
+   * Lehnt den Match zu einer gegebenen MatchId vom aktuellen User aus ab
+   * @param {matchId} String die Id des Matches
+   * @returns {Promise|JSON} welches angibt ob die Aktion erfolgreich war und das
+   * aktualisierte User Objekt enthält
    */
   declineMatch (matchId) {
-    // return db.modules.post('declineMatch', {id: matchId})
     return db.modules.post('MatchService', {
       route: 'DECLINE_MATCH',
       id: matchId
@@ -365,7 +392,8 @@ class Modul {
   }
 
   /**
-    gibt einen Teststring aus
+    gibt einen Teststring aus. Zum testen der Einbindung des Moduls.
+    @return {String}
   */
   getTestString () {
     return 'Test String'
